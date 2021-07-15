a1303d2fa629400768b772584b53a7b2730a277f
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-07-15 16:00:07 +0200

    add common method used for incoming tools

---
+++ common_.py
@@ -0,0 +1,60 @@
+import substance_painter
+
+from CommonTools.concat import concat
+
+
+def test():
+    print("")
+
+
+def qt_instance():
+    return substance_painter.ui
+
+
+def main_window():
+    return qt_instance().get_main_window()
+
+
+def filepath():
+    return substance_painter.project.file_path()
+
+
+def save_as(filepath_):
+    substance_painter.project.save_as(filepath_, substance_painter.project.ProjectSaveMode.Full)
+
+
+def open_file(filepath_):
+    substance_painter.project.open(filepath_)
+
+
+def normal_map_format():
+    return substance_painter.project.NormalMapFormat.OpenGL
+
+
+def tangent_space():
+    return substance_painter.project.TangentSpace.PerVertex
+
+
+def uv_tile_workflow():
+    return substance_painter.project.ProjectWorkflow.UVTile
+
+
+def project_settings():
+    settings = substance_painter.project.Settings(import_cameras=False,
+                                                  normal_map_format=normal_map_format(),
+                                                  tangent_space_mode=tangent_space(),
+                                                  project_workflow=uv_tile_workflow(),
+                                                  default_texture_resolution=2048)
+
+    return settings
+
+
+def mesh_file(root, project, type_, name_, quality="LD"):
+    filename = concat(name_, "_MOD_", quality, ".obj")
+    return concat(root, project, "DATA/LIB/PUBLISH", type_, name_, "MOD", quality, filename,
+                  separator="/")
+
+
+def create_project(mesh):
+    substance_painter.project.create(mesh_file_path=mesh,
+                                     settings=project_settings())
